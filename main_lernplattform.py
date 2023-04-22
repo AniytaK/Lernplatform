3dd774cd235f80995012111eb62d1049c31ba45e
Weissgerber Alexandra <weissgalexan@fhgr.ch>
2023-04-22 14:05:56 +0200

    Objektreferenz

--- main_lernplattform.py
+++ main_lernplattform.py
@@ -1,4 +1,13 @@
 from aufgabe import Aufgabe
 a1 = Aufgabe('25.4.2023', '27.4.2023')
 
+print(a1)
+a2 = a1
+print(a2)
+
+a3 = Aufgabe('25.5.2023', '27.5.2023')
+print(a3)
+a2.startdatum = "12.23.2345"
 print(a1.startdatum)
+print(a2.startdatum)
+print(a3.startdatum)
