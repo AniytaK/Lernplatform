183de6984416a57b0ad34e688dc6e3c521f58e04
lydia-sa <131520604+lydia-sa@users.noreply.github.com>
2023-04-22 14:00:30 +0200

    klasse "aufgabe" angepasst

--- aufgabe.py
+++ aufgabe.py
@@ -1,4 +1,10 @@
 class Aufgabe:
     def __init__(self, startdatum, enddatum):
         self.startdatum = startdatum
-        self.startdatum = enddatum
+        self.enddatum = enddatum
+
+    def __str__(self):
+        return f"Startdatum: {self.startdatum}, Enddatum: {self.enddatum}"
+
+    def drucken(self):
+        print("Hello die Aufgabe wird am " + self.enddatum + " enden.")
--- main_lernplattform.py
+++ main_lernplattform.py
@@ -1,4 +1,5 @@
 from aufgabe import Aufgabe
 a1 = Aufgabe('25.4.2023', '27.4.2023')
 
-print(a1.startdatum)
+print(a1)
+a1.drucken()
