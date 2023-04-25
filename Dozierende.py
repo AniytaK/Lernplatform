a1b30a95bd453039926567b2bd4acb5bee4662e1
AniytaK <anna.kuriger@gmail.com>
2023-04-25 22:25:28 +0200

    new

--- Dozierende.py
+++ Dozierende.py
@@ -1,8 +1,16 @@
 
 class Dozierende:
-    def __init__(self, name):
+    def __init__(self, vorname, name, kurs):
+        self.vorname = vorname
         self.name = name
+        self.kurs = kurs
 
-p1 = Dozierende("Max")
+    def sich_vorstellen(self):
+        print("Mein Name ist " +self.vorname+" " + self.name)
+        print("Ich bin Dozent in " +self.kurs + " Kurs")
 
-print(p1.name)
+d1 = Dozierende("Muster", "Max", "Python")
+d2 = Dozierende("Kunt", "Tom", "Design")
+
+d1.sich_vorstellen()
+d2.sich_vorstellen()
